21400274c27d145eb7c5acdce7d8d8815ddfd514
LaurentiuMa <l.maties29@yahoo.com>
2020-12-11 15:57:37 +0000

    Jupyter notebook
    Basic explanation on how the code works if you're stuck.

--- testFile.py
+++ testFile.py
@@ -1,6 +1,7 @@
 import os
+import sys
 import pandas as pd
-from cleanedData import Cleaner
+from . cleanedData import Cleaner
 
 cleaner = Cleaner(r"C:\Users\Laurentiu\OneDrive - City, University of London\IN3062 - Introduction to Artificial Intelligence\Coursework")
 
