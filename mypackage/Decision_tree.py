0f1eda5a6c5e68ae3e4e8c951b120d38a245a706
Patryk Owczarz <powczarz76d@gmail.com>
2020-12-21 22:27:29 +0000

    Decision tree hyperparameter training and code optimisation
    Implemented hyperparameter training for tree models and worked on commenting the code for the final submission.

--- mypackage/Decision_tree.py
+++ mypackage/Decision_tree.py
@@ -8,6 +8,89 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 
 from sklearn import metrics
+from sklearn.metrics import r2_score
+
+def HyperParameterTraining(path):
+    #Change this path to a path relative to where u want to store the results.
+    results_file = path
+
+    max_depth_vals = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    min_sample_split_vals = [2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+    best_r2_score = 0
+    best_max_depth = 0
+    best_min_split_sample = 0
+
+    for d in max_depth_vals:
+        for sample in min_sample_split_vals:
+            dt_model = DecisionTreeRegressor(criterion= "mse", random_state= 42, 
+                                             max_depth= d, min_samples_split= sample)
+            dt_model.fit(X_train,y_train)
+            
+            y_pred_dt = dt_model.predict(X_test)
+            print('\n')
+            print("max_depth = {0}, min_sample_split = {1}".format(d, sample))
+            print('Mean:', np.mean(y_test))
+            print('Decision tree RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred_dt)))
+            r2 = r2_score(y_test,y_pred_dt)
+            print('Decision tree R2:', r2)
+            
+            if r2 > best_r2_score:
+                best_r2_score = r2
+                best_max_depth = d
+                best_min_split_sample = sample
+                
+            file = open(results_file, "a")
+            file.write("\n")
+            file.write("max_depth = {0}, min_sample_split = {1}".format(d, sample))
+            file.write("\n")
+            file.write("Mean: {0}".format(np.mean(y_test)))
+            file.write("\n")
+            file.write("Decision tree RMSE: {0}".format(np.sqrt(metrics.mean_squared_error(y_test, y_pred_dt))) )
+            file.write("\n")
+            file.write("Decision tree R2: {0}".format(r2_score(y_test,y_pred_dt)))
+            file.write("\n")
+            file.write("================================================================")
+            file.close()
+                
+    print('\n')
+    print("==========================================================================")
+    print("The best results are:")
+    print("r2_score = {0}, max_depth = {1}, min_split_sample = {2}".format(best_r2_score, best_max_depth, best_min_split_sample))
+    print("==========================================================================")
+                
+    return
+
+
+def RandomForestHyperParameterTraining(max_n):
+    #investigates the RMSE over a range of estimators plotting the result
+    #This code might take a while to run depending on the value of max_n
+    #This code was taken from Week 5 exercises part 2. Tweaked the code to test for RMSE and using the
+    #RandomForestRegressor model instead of the classifier model.
+    
+    fig = plt.figure(figsize=(15,10))
+    
+    rmse_data = []
+    nums = []
+
+    for i in range(1,max_n + 1):
+        rf_model = RandomForestRegressor(n_estimators=i, max_depth=13, min_samples_split=13,
+                                         criterion="mse", random_state=42)
+        rf_model.fit(X_train, y_train)
+        y_model = rf_model.predict(X_test)
+        rmse = np.sqrt(metrics.mean_squared_error(y_test, y_model))
+        rmse_data.append(rmse)
+        nums.append(i)
+
+    print(rmse_data)
+    plt.plot(nums,rmse_data)
+    plt.xlabel("Number of Trees (n_estimators)")
+    plt.ylabel("RMSE")
+    plt.show()
+
+    return
+
+
 
 #initialised the data cleaner class from the folder.
 cleaner = Cleaner(r"D:\Uni Stuff\Modules\IN3062 Intro to AI\IN3062-Coursework")
@@ -15,73 +98,99 @@ cleaner = Cleaner(r"D:\Uni Stuff\Modules\IN3062 Intro to AI\IN3062-Coursework")
 #built the dataframe from the cleaned data.
 df = cleaner.getDataFrame()
 
+#testing if the dataframe has been read successfully.
 print(df[:5])
 
-# =============================================================================
-# data = []
-# for x in df.columns:
-#     if x == 'carat': 
-#         data.append(x)
-# =============================================================================
-
-diamond_features = ['carat','x','y','z','color','cut','clarity']
+#list of features that we are analysing
+diamond_features = ['carat','x','y','z','color','cut','clarity','depth','table']
 X = df.loc[:, diamond_features].values
-#df_price = np.log(df['price']) #This logs the price. Purpose described in the report.
 
-#X = df[data].values
-y = df['price'].values
+#This logs the price. Purpose described in the report.
+df_price = np.log(df['price'])
 
+#the dataset is split into training and testing datasets.
+X_train, X_test, y_train, y_test = train_test_split(X, df_price, test_size=0.20, random_state=42)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+#uncomment this function call for the Decision Tree Hyperparameter testing, change the path for where you want to store the .txt file.
+#HyperParameterTraining(r"D:\Hyperparam Training\Decision_Tree_Results.txt")
 
-#fig = plt.figure(figsize=(15,10))
-
-#investigates the RMSE over a range of estimators plotting the result
-#THIS TAKES A WHILE TO RUN!!
-#This code was taken from Week 5 exercises part 2. Tweaked the code to test for RMSE and using the
-#RandomForestRegressor model instead of the classifier model.
-# =============================================================================
-# rmse_data = []
-# nums = []
-# for i in range(1,128):
-#     rf_model = RandomForestRegressor(n_estimators=i,criterion="mse")
-#     rf_model.fit(X_train, y_train)
-#     y_model = rf_model.predict(X_test)
-#     rmse = np.sqrt(metrics.mean_squared_error(y_test, y_model))
-#     rmse_data.append(rmse)
-#     nums.append(i)
-#     
-# print(rmse_data)
-# plt.plot(nums,rmse_data)
-# plt.xlabel("Number of Trees (n_estimators)")
-# plt.ylabel("RMSE")
-# plt.show()
-# =============================================================================
-
-dt_model = DecisionTreeRegressor()
+#initialising the model based on the hyperparameter training and fitting the model to the dataset
+dt_model = DecisionTreeRegressor(criterion= "mse", random_state= 42, 
+                                 max_depth= 13, min_samples_split= 13)
 dt_model.fit(X_train,y_train)
 
-rf_model = RandomForestRegressor(n_estimators=31, criterion="mse", bootstrap=True)
+#uncomment this function call for the Random Forest n_estimator testing, change the parameter to set the max n_estimator value for training.
+#RandomForestHyperParameterTraining(100)
+
+#initialising the model based on the hyperparameter training and fitting the model to the dataset
+rf_model = RandomForestRegressor(n_estimators=21, max_depth=13, min_samples_split=13,
+                                 criterion="mse", bootstrap=True, random_state=42)
 rf_model.fit(X_train, y_train)
 
+#Using the testing dataset to predict the price of the diamonds.
 y_pred_dt = dt_model.predict(X_test)
 y_pred_rf = rf_model.predict(X_test)
 
-df_compare = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred_dt})
+#comparing the actual values to the predicted values.
+df_compare = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred_dt, 'Actual exp': np.exp(y_test), 'Predicted exp': np.exp(y_pred_dt)})
 df_head = df_compare.head(25)
 print(df_head)
+
+#printing the mean, RMSE for both the Decision Tree and Random Forest model and the R2 values for both models.
 print('\n')
 print('Mean:', np.mean(y_test))
 print('Decision tree RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred_dt)))
+print('Decision tree R2:', r2_score(y_test,y_pred_dt))
 print('Random Forest RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred_rf)))
+print('Decision tree R2:', r2_score(y_test,y_pred_rf))
 
 carat_test = X_test[:,:1]
 
-
+#plotting a scatter diagram of actual test data and the predicted values from the Decision Tree model based on the carat feature.
 plt.figure(1)
 plt.scatter(carat_test, y_test, s=1, color='red')
 plt.scatter(carat_test, y_pred_dt, s=1, color='blue')
 
+#plotting a scatter diagram of actual test data and the predicted values from the Random Forest model based on the carat feature.
 plt.figure(2)
 plt.scatter(carat_test, y_test, s=1, color='red')
 plt.scatter(carat_test, y_pred_rf, s=1, color='blue')
+
+
+
+
+# =============================================================================
+# #This code would find the optimal values for max_depth, min_sample_split and the n_estimators
+# #Takes an extremely long time to compute, would have to run it over night, ran out of time to test.
+#
+# max_depth_vals = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+# min_sample_split_vals = [2,3,4,5,6,7,8,9,10]
+# 
+# best_r2_score = 0
+# best_max_depth = 0
+# best_min_split_sample = 0
+# best_n_estimators = 0;
+# 
+# for i in range(1,40):
+#     for d in max_depth_vals:
+#         for sample in min_sample_split_vals:
+#             rf_model = RandomForestRegressor(n_estimators=i, max_depth=d, min_samples_split=sample,
+#                                              criterion="mse", random_state=42)
+#             rf_model.fit(X_train, y_train)
+#             y_model = rf_model.predict(X_test)
+#             #rmse = np.sqrt(metrics.mean_squared_error(y_test, y_model))
+#             r2 = r2_score(y_test, y_model)
+#             #rmse_data.append(rmse)
+#             #nums.append(i)
+#             if r2 > best_r2_score:
+#                 best_r2_score = r2
+#                 best_max_depth = d
+#                 best_min_split_sample = sample
+#                 best_n_estimators = i
+#     
+# print('\n')
+# print("==========================================================================")
+# print("The best results are:")
+# print("r2_score = {0}, max_depth = {1}, min_split_sample = {2}, n_estimators = {3}".format(best_r2_score, best_max_depth, best_min_split_sample,best_n_estimators))
+# print("==========================================================================")
+# =============================================================================
--- mypackage/Linear_regression.py
+++ mypackage/Linear_regression.py
@@ -6,10 +6,10 @@ from cleanedData import Cleaner
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MinMaxScaler
+#from sklearn.preprocessing import StandardScaler
 
 from sklearn import metrics
+from sklearn.metrics import r2_score
 
 #initialised the data cleaner class from the folder.
 cleaner = Cleaner(r"D:\Uni Stuff\Modules\IN3062 Intro to AI\IN3062-Coursework")
@@ -19,26 +19,17 @@ df = cleaner.getDataFrame()
 
 print(df[:5])
 
-#scaler = MinMaxScaler()
-#df[df.columns] = scaler.fit_transform(df[df.columns])
-
-data = []
-for x in df.columns:
-    if x == 'carat': 
-        data.append(x)
-
-diamond_features = ['carat','x','y','z','color','cut','clarity']
+diamond_features = ['carat','x','y','z','color','cut','clarity','depth','table']
 X = df.loc[:, diamond_features].values
 
-#X = df[data].values
+#normal price value instead of log of the price.
 #y = df['price'].values
 
-#print(X[:5])
-#print(y[:5])
-
+#The dependent feature is the log of the price.
 df_price = np.log(df['price'])
 
-X_train, X_test, y_train, y_test = train_test_split(X, df_price, test_size=0.20, random_state=42)
+#splitting the dataset into train and test datasets.
+X_train, X_test, y_train, y_test = train_test_split(X, df_price, test_size=0.25, random_state=42)
 
 #Linear Regression Model
 lr_model = LinearRegression(fit_intercept = True)
@@ -60,51 +51,94 @@ pr_model2.fit(X_poly_train2, y_train)
 lr_model3 = LinearRegression()
 lr_model3.fit(X_poly_train2, y_train)
 
-#Polynomial Regression Model, Degree 4
-pr_model3 = PolynomialFeatures(degree=4)
-X_poly_train3 = pr_model3.fit_transform(X_train)
-X_poly_test3 = pr_model3.fit_transform(X_test)
-pr_model3.fit(X_poly_train3, y_train)
-lr_model4 = LinearRegression()
-lr_model4.fit(X_poly_train3, y_train)
+# =============================================================================
+# #Not used because of poor results and long computational time.
+# #Polynomial Regression Model, Degree 4
+# pr_model3 = PolynomialFeatures(degree=4)
+# X_poly_train3 = pr_model3.fit_transform(X_train)
+# X_poly_test3 = pr_model3.fit_transform(X_test)
+# pr_model3.fit(X_poly_train3, y_train)
+# lr_model4 = LinearRegression()
+# lr_model4.fit(X_poly_train3, y_train)
+# =============================================================================
 
-print(lr_model.coef_)
-
+#The prediction values for all the models
 y_pred = lr_model.predict(X_test)
 y_pred2 = lr_model2.predict(X_poly_test)
 y_pred3 = lr_model3.predict(X_poly_test2)
-y_pred4 = lr_model4.predict(X_poly_test3)
 
-df_compare = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+# =============================================================================
+# #The predicted value for a polynomial model of degree 4
+# y_pred4 = lr_model4.predict(X_poly_test3)
+# =============================================================================
+
+# =============================================================================
+# #This code is to find outliers that give bad results in the polynomial regression.
+# print('Error testing degree 3:')
+# print(diamond_features)
+# for i in range(1, len(y_pred3)):
+#     if y_pred3[i] > 12:
+#         print(X_test[i])
+# 
+# print('\n')
+
+# print('Error testing degree 2:')
+# print(diamond_features)
+# for i in range(1, len(y_pred2)):
+#     if y_pred2[i] > 11:
+#         print(X_test[i])
+# 
+# print('\n')
+# =============================================================================
+
+
+#comparing actual to predicted values.
+df_compare = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred, 'Actual exp': np.exp(y_test), 'Predicted exp': np.exp(y_pred)})
 df_head = df_compare.head(25)
 print(df_head)
 print('\n')
 print('Mean:', np.mean(y_test))
 print('Linear Regression RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+print('Linear Regression R2:', r2_score(y_test,y_pred))
 print('Polynomial Regression degree 2 RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred2)))
+print('Polynomial Regression degree 2 R2:', r2_score(y_test,y_pred2))
 print('Polynomial Regression degree 3 RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred3)))
-print('Polynomial Regression degree 4 RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred4)))
-#print('Amount of error: ', (np.sqrt(metrics.mean_squared_error(y_test,y_pred))/np.mean(y_test)) * 100, '%' )
+print('Polynomial Regression degree 3 R2:', r2_score(y_test,y_pred3))
+# =============================================================================
+# #RMSE and R2 values for polynomial regression degree 4
+# print('Polynomial Regression degree 4 RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred4)))
+# print('Polynomial Regression degree 4 R2:', r2_score(y_test,y_pred4))
+# =============================================================================
 
-#plt.scatter(X_test, y_test, c='red')
-#plt.scatter(X_test,y_pred2, c='green')
-#plt.show()
-
+#Takes the carat values out of the test dataset to compare to our predicted values.
 carat_test = X_test[:,:1]
 
-
+#The next 4 graphs plot the predicted and actual price results against the carat value. Reason for carat in report.
+#Red is actual values, blue is predicted values.
+#Plots 2 scatter graphs on top of one another, results are for linear regression.
 plt.figure(1)
 plt.scatter(carat_test, y_test, s=1, color='red')
 plt.scatter(carat_test, y_pred, s=1, color='blue')
+plt.xlabel("Carat")
+plt.ylabel("Log(price)")
 
+#Plots 2 scatter graphs on top of one another, results are for polynomial regression degree 2. 
 plt.figure(2)
 plt.scatter(carat_test, y_test, s=1, color='red')
 plt.scatter(carat_test, y_pred2, s=1, color='blue')
+plt.xlabel("Carat")
+plt.ylabel("Log(price)")
 
+#Plots 2 scatter graphs on top of one another, results are for polynomial regression degree 3. 
 plt.figure(3)
 plt.scatter(carat_test, y_test, s=1, color='red')
 plt.scatter(carat_test, y_pred3, s=1, color='blue')
+plt.xlabel("Carat")
+plt.ylabel("Log(price)")
 
-plt.figure(4)
-plt.scatter(carat_test, y_test, s=1, color='red')
-plt.scatter(carat_test, y_pred4, s=1, color='blue')
+# =============================================================================
+# #Plots the results for polynomial regression degree 4 for the price against carat.
+# plt.figure(4)
+# plt.scatter(carat_test, y_test, s=1, color='red')
+# plt.scatter(carat_test, y_pred4, s=1, color='blue')
+# =============================================================================
